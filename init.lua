248f3ee7eb4a875f36dcdd2355991c0a05b1cdc0
Tombstone8140 <lachlanralsop@gmail.com>
2025-06-09 15:22:33 +1000

    decided against telescope for harpoon, changed prev and next buffer, bug crashing it but mostly works.

--- init.lua
+++ init.lua
@@ -503,10 +503,10 @@ require('lazy').setup({
       end)
 
       -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set('n', '<C-S-P>', function()
+      vim.keymap.set('n', '<leader>p', function()
         harpoon:list():prev()
       end)
-      vim.keymap.set('n', '<C-S-N>', function()
+      vim.keymap.set('n', '<leader>n', function()
         harpoon:list():next()
       end)
 
